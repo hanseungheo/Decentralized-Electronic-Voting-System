5268702675397fd8110b42731ed4e31763a3a1fd
hanseungheo <martinhan486@naver.com>
2022-10-15 17:56:02 +0900

    Update 04.block_hash.py

--- lab_blockchain/04.block_hash.py
+++ lab_blockchain/04.block_hash.py
@@ -1,10 +1,6 @@
 import hashlib
 
-a = 'abcd'
-a_hash = hashlib.sha256(a.encode()).hexdigest()
-print(a_hash)
-print(len(a_hash))
-
+chain = []
 
 def get_block_hash(block):
     data = dict()
@@ -12,3 +8,34 @@ def get_block_hash(block):
     data['data'] = sorted(block['transaction']['data'].copy().items())
     data = sorted(data.items())
     return hashlib.sha256(str(data).encode()).hexdigest()
+
+
+block1 = {
+    'transaction' : {
+        'type' : 'open',
+        'data' : {
+            'id' : '투표 ID',
+            'question':'투표 질문',
+            'options': ['투표 항목1', '투표 항목2', '투표 항목3']
+        }
+    }
+}
+
+block1['hash'] = get_block_hash(block1)
+
+chain.append(block1)
+
+block2 = {
+    'transaction': {
+        'type': 'vote',
+        'data': {
+            'id': '투표 ID',
+            'vote': '투표 항목'
+        }
+    }
+}
+block2['hash'] = get_block_hash(block2)
+
+chain.append(block2)
+
+print(chain)
